051d949961712f7fc77d370a005caaa15c6f8498
rimuru322 <nikitka228t@gmail.com>
2025-06-12 04:23:16 +0300

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,1 +1,6 @@
-pyinstaller --noconfirm --onefile --windowed --add-data "images;images/"  "main.py"
+if exist "icon.ico" (
+        pyinstaller --noconfirm --onefile --windowed --icon "icon.ico" --add-data "images;images/"  "main.py"
+    ) else (
+        pyinstaller --noconfirm --onefile --windowed --add-data "images;images/"  "main.py"
+        echo WARNING: icon.ico doesn't exist!
+    )
